a43bc2b67994fe2c99863dd22a6d0b46534fe1e4
webz <zilvism@gmail.com>
2025-05-23 21:08:16 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const genDOM=document.querySelector(".btn");
 const inputDOM=document.getElementById("input");
-
+const copyEl=document.querySelector(".fa-copy");
 
 genDOM.addEventListener("click",  ()=> {    
-    const select="abcd21";
+    const select="abcdefghijklmonpstuvyz[].!";
     let pass="";
-    for (i=0; i<=5; i++) {
-        const num=Math.floor(Math.random()*5)
+    for (i=0; i<7; i++) {
+        const num=Math.floor(Math.random()*(select.length-1))
         pass+=select[num];
         console.log('test')    
     }
     inputDOM.value=pass;
+});
+
+copyEl.addEventListener('click', ()=>{
+    inputDOM.select();
+    inputDOM.setSelectionRange(0, 999);
+    navigator.clipboard.writeText(inputDOM.value);
 });
