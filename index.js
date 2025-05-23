ee5c740c25ba0cd36328aa0887f750dfd15610a2
webz <zilvism@gmail.com>
2025-05-23 13:55:30 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const genDOM=document.querySelector('.btn');
-const getDOM=document.getElementById('input');
+const inputDOM=document.querySelector('.input');
 
-genDOM=document.addEventListener('click', passw => {
-    passw.preventDefault();
-    getDOM.innerHTML=`<h1 class="password">${passw}</h1>`;
-    let passw='';
-    
+genDOM=document.addEventListener('click',  () => {    
     const select='abcd21';
     for (i=0; i<=5; i++) {
         const num=Math.floor(Math.random()*6)
-        passw+=select[num];    
+        inputDOM.value+=select[num];    
     }
 });
